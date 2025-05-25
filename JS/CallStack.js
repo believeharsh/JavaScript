0b1c6f0dd5e208f7514d30a16962890e447609d2
Believeharsh <bontinue@gmail.com>
2025-05-25 15:16:04 +0530

    JS Practice

---
+++ JS/CallStack.js
@@ -0,0 +1,7 @@
+// what is the callStack in javaScript??
+// callStack is a data structure that keeps track of the execution context of the function calls.
+
+// Browser's Execution context will be different as compare to the Node js execution context 
+// 1. Global Execution context 
+// 2. Function Execution context 
+// 3. Eval Execution context ; 
